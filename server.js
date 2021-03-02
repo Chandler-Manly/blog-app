4a5448dedd332c26ab05fdea59cc1ed436ba1599
Jessica Yee <jessicayee@Jessicas-MacBook-Pro.local>
2021-03-02 15:19:06 -0500

    remove node modules

---
+++ server.js
@@ -0,0 +1,28 @@
+// const express = require('express');
+// const cors = require('cors')
+// const bodyParser = require('body-parser');
+// const logger = require('morgan');
+
+// //TODO: CHANGE THIS!!
+// const productsRoutes = require('./routes/products');
+
+
+
+// const db = require('./db/connection')
+// const PORT = process.env.PORT || 3000
+
+// const app = express();
+
+// app.use(cors())
+// app.use(bodyParser.json())
+
+// //! WHAT IS DEV REFERRING TO?
+// app.use(logger('dev'))
+
+
+// //TODO: CHANGE THIS!!
+// app.use('/api', productsRoutes);
+
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+// app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
