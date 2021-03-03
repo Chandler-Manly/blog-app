163dfed825c042491738a23d44d1f8a18672b93b
Shamma Qureshi <shammaqureshi@Shammas-MacBook-Pro.local>
2021-03-03 15:21:56 -0500

    Imported Screens and created routes in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
+import React from "react";
+import Home from "./screens/Home/Home.jsx";
+import Posts from "./screens/Posts/Posts.jsx";
+import CreatePost from "./screens/CreatePost/CreatePost.jsx";
+import EditPost from "./screens/EditPost/EditPost.jsx";
+import PostDetails from "./screens/PostDetails/PostDetails.jsx";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
-import "/src/components/shared/Layout/Layout";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Team Jess</h1>
-      <Layout />
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/posts" component={Posts} />
+        <Route path="/add-post" component={CreatePost} />
+        <Route exact path="/posts/:id/edit" component={EditPost} />
+        <Route exact path="/posts/:id" component={PostDetails} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
