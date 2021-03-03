2123806425f431df9cbf7ef15778d485d64b98bc
HC <manlychandler@gmail.com>
2021-03-03 15:33:29 -0500

    commented out the sort functions

--- client/src/screens /Posts/Posts.jsx
+++ client/src/screens /Posts/Posts.jsx
@@ -9,7 +9,7 @@ import { getPosts } from '../../services/posts';
 const Posts = (props) => {
   const [allPosts, setAllPosts] = useState([])
   const [queriedPosts, setQueriedPosts] = useState([])
-  const [sortType, setSortType] = useState([])
+  // const [sortType, setSortType] = useState([])
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -22,35 +22,35 @@ const Posts = (props) => {
 
   // Comment: If we want to sort our blog post by date, popularity (like button) or another method this can be used for starter code!
   
-  const handleSort = type => {
-    setSortType(type)
-    switch (type) {
-      case "author-ascending":
-        setQueriedPosts(AZ(queriedPosts))
-        break
-      case "author-descending":
-        setQueriedPosts(ZA(queriedPosts))
-        break
-      default:
-        break
-    }
-  }
+  // const handleSort = type => {
+  //   setSortType(type)
+  //   switch (type) {
+  //     case "author-ascending":
+  //       setQueriedPosts(AZ(queriedPosts))
+  //       break
+  //     case "author-descending":
+  //       setQueriedPosts(ZA(queriedPosts))
+  //       break
+  //     default:
+  //       break
+  //   }
+  // }
 
   const handleSearch = event => {
     const newQueriedPosts = allPosts.filter(post => post.author.toLowerCase().includes(event.target.value.toLowerCase()))
-    setQueriedPosts(newQueriedPosts, () => handleSort(sortType))
+    setQueriedPosts(newQueriedPosts)
   }
 
   const handleSubmit = event => event.preventDefault()
 
   const postsJSX = queriedPosts.map((post, index) =>
-    <Post _id={post._id} name={post.author} imgURL={post.imgURL} content={post.content} key={index} />
+    <Post _id={post._id} title={post.title} author={post.author} imgURL={post.imgURL} content={post.content} key={index} />
   )
 
   return (
     <Layout>
       <Search onSubmit={handleSubmit} onChange={handleSearch} />
-      <Sort onSubmit={handleSubmit} onChange={handleSort} />
+      {/* <Sort onSubmit={handleSubmit} onChange={handleSort} /> */}
       <div className="posts">
         {postsJSX}
       </div>
