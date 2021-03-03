df0a958f3efe1e34399405fe1fb404cab484bef5
Shamma Qureshi <shammaqureshi@Shammas-MacBook-Pro.local>
2021-03-03 15:02:36 -0500

    Added home screen to render product cards

---
+++ client/src/screens /Home/Home.jsx
@@ -0,0 +1,15 @@
+import PostCards from "../../components/PostCards/PostCards";
+import Layout from "../../components/shared/Layout/Layout";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="home">
+        <PostCards />
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
