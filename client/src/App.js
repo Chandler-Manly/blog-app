80006114b3908b1ec002390387310ae43ebb44b2
HC <manlychandler@gmail.com>
2021-03-04 16:08:22 -0500

    updated css

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Home from "./screens/Home/Home.jsx";
 import Posts from "./screens/Posts/Posts.jsx";
 import CreatePost from "./screens/CreatePost/CreatePost.jsx";
