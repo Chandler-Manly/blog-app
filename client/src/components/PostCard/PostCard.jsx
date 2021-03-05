cfcc3995113d7964c2f44553f7c53e2e8a32a844
Jessica Yee <jessicayee@Jessicas-MacBook-Pro.local>
2021-03-04 20:20:20 -0500

    fix className dupe

--- client/src/components/PostCard/PostCard.jsx
+++ client/src/components/PostCard/PostCard.jsx
@@ -8,10 +8,10 @@ const PostCard = (props) => {
         <div className="left-card-container">
           <div className="top-half">
 
-            <div className="post-title">{props.title}</div>
+            <div className="post-title-home">{props.title}</div>
             <div className="post-author">{props.author}</div>
           </div>
-          
+
           <div className="view-statement">View Post</div>
         </div>
         <img className="post-card-image" src={props.imgURL} alt={props.name} />
