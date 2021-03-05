911adbf27262e9f04153b9906ad7eb165311c339
Jessica Yee <jessicayee@Jessicas-MacBook-Pro.local>
2021-03-04 19:59:31 -0500

    edit homepage cards css

--- client/src/components/PostCard/PostCard.jsx
+++ client/src/components/PostCard/PostCard.jsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
-// import "./Postcard.css";
+import "./PostCard.css";
 
 const PostCard = (props) => {
   return (
     <div className="post-card">
       <Link className="card" to={`/posts/${props._id}`}>
-        <div className="post-title">{props.title}</div>
-        <div className="post-author">{props.author}</div>
+        <div className="left-card-container">
+          <div className="top-half">
+
+            <div className="post-title">{props.title}</div>
+            <div className="post-author">{props.author}</div>
+          </div>
+          
+          <div className="view-statement">View Post</div>
+        </div>
         <img className="post-card-image" src={props.imgURL} alt={props.name} />
-        <div>View Post</div>
       </Link>
     </div>
   );
